c5af5d46bfadefa54f5f85599762e69f49dd95b3
msliwka <s12454@pjwstk.edu.pl>
2017-01-26 20:29:11 +0100

    nowy main

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <ios>
+#include <stdexcept>
+#include <Windows.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/core/core.hpp>
+
